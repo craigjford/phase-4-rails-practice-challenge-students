de068141f9715fc502a27962ca93a3b61351b1b6
Craig Ford <craig_ford@icloud.com>
2022-11-03 21:15:19 -0400

    75 minutes

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,51 @@
+class StudentsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    wrap_parameters format: []
+    
+    def index        
+        students = Student.all  
+        render json: students
+    end
+
+    def show    
+        student = find_student
+        render json: student
+    end
+
+    def update   
+        student = find_student
+        student.update!(student_params)
+        render json: student, status: :ok
+    end
+
+    def create   
+        student = Student.create!(student_params)
+        render json: student, status: :created
+    end
+
+    def destroy 
+        student = find_student  
+        student.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_student   
+        Student.find(params[:id])
+    end
+
+    def student_params
+        params.permit(:name, :major, :age)
+      end
+
+    def render_not_found_response
+      render json: { error: "Review not found" }, status: :not_found
+    end
+  
+    def render_unprocessable_entity_response(invalid)  
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+end
---
+++ app/models/student.rb
@@ -0,0 +1,8 @@
+class Student < ApplicationRecord
+
+  validates :name, presence: true
+  validates :age, numericality: { only_integer: :true, greater_than_or_equal_to: 18 }
+
+  belongs_to :instructor, dependent: :destroy
+
+end
